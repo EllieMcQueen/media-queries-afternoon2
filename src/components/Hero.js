140378776f1032a447ac0af6ba5e1193063ac25e
Ellie McQueen <elliemcqueen@Ellies-MacBook-Pro.local>
2020-09-28 17:43:40 -0600

    update

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Hero extends Component{
+    render(){
+        return(
+            <section className="hero">
+            <h2>G R A Y S C A L E</h2>
+            <p>A free, responsive, one page Bootstrap theme<br/> created by Start Bootstrap.</p>
+              <button class="button">GET STARTED</button>
+            
+             </section>
+        )
+    }
+}
+export default Hero;
